dd2a78d8cf51478d1c66e684b17d36837b62826f
Sankalp Sagar <48313574+sankalpsagar@users.noreply.github.com>
2021-09-27 13:25:29 +0530

    updated multicast example
    forgot to save

--- Program.cs
+++ Program.cs
@@ -4,9 +4,28 @@ namespace multicastdemo
 {
     class Program
     {
+        static void Test() { }
+
+        static void NewTest() { }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Action funObj1 = new Action(Program.Test);
+            Action funObj2 = new Action(Program.NewTest);
+
+            //funObj1 = (Action) System.Delegate.Combine(funObj1, funObj2);
+
+            funObj1 = funObj1 + funObj2;
+            Broadcast(funObj1);
+
+            //can also do: funObj1 = funObj1 - funObj2
+
+        }
+
+        static void Broadcast (Action funcObj)
+        {
+            funcObj.Invoke();
         }
     }
 }
